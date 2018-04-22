f8bb638758035ad8226615b5fa01d3935cece1fc
Mayank Kushal <mayankkushal26@gmail.com>
2018-04-22 22:51:43 +0530

    Minor fixes

--- fyle_project/settings.py
+++ fyle_project/settings.py
@@ -16,7 +16,9 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
